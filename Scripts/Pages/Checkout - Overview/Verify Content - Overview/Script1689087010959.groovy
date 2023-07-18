7ac93ca0eb89fa687cf80ec9dd6f9d83b02dab27
ayuanriyani <ayuanriyani@gmail.com>
2023-07-18 20:35:00 +0700

    initialize

---
+++ Scripts/Pages/Checkout - Overview/Verify Content - Overview/Script1689087010959.groovy
@@ -0,0 +1,49 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.verifyElementVisible(findTestObject('checkout_Page2_overview/button_finish'), 0)
+
+Mobile.verifyElementVisible(findTestObject('checkout_Page2_overview/button_cancel_checkout'), 0)
+
+Mobile.verifyElementVisible(findTestObject('checkout_Page2_overview/button_back_product'), 0)
+
+Mobile.verifyElementVisible(findTestObject('checkout_Page2_overview/txt_QTY'), 0)
+
+Mobile.verifyElementVisible(findTestObject('checkout_Page2_overview/Total_QTY'), 0)
+
+Mobile.verifyElementVisible(findTestObject('checkout_Page2_overview/txt_DESCRIPTION'), 0)
+
+Mobile.verifyElementVisible(findTestObject('checkout_Page2_overview/Title_deskripsi_product'), 0)
+
+Mobile.verifyElementVisible(findTestObject('checkout_Page2_overview/Price_QTY'), 0)
+
+Mobile.verifyElementVisible(findTestObject('checkout_Page2_overview/txt_Payment_Information'), 0)
+
+Mobile.verifyElementVisible(findTestObject('checkout_Page2_overview/Info_Payment_Information'), 0)
+
+Mobile.verifyElementVisible(findTestObject('checkout_Page2_overview/txt_Shipping_Information'), 0)
+
+Mobile.verifyElementVisible(findTestObject('checkout_Page2_overview/Info_Shipping_Information'), 0)
+
+Mobile.verifyElementVisible(findTestObject('checkout_Page2_overview/item_total_checkout'), 0)
+
+Mobile.verifyElementVisible(findTestObject('checkout_Page2_overview/tax_checkout'), 0)
+
+Mobile.verifyElementVisible(findTestObject('checkout_Page2_overview/Total'), 0)
+
---
+++ Test Suites/login.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>44304b84-95ba-4606-81cc-f18d52b9e13d</testSuiteGuid>
+   <testCaseLink>
+      <guid>4810bc80-1450-4288-9f2a-9db5c639c71e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature - Login/ULG004 - User login use data password wrong</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6eea810a-ecf9-42ab-8fc3-88c288d45b70</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature - Login/UGL003 - User login use data username empty</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>836ceda7-abd3-4f83-afbf-3e7a337eb9d5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature - Login/ULG002 - User login use data empty</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7c9a2f76-18d6-42f5-bad4-027d2f02e08e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature - Login/ULG001 - User login successfully</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
